c0afa3362c5002aa172d7fe2f587ced60d2c76cd
abhijeet080808 <abhijeet080808@gmail.com>
2020-02-06 21:01:48 +0530

    Read asn file

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -1,12 +1,28 @@
 #include "Parser.hh"
 
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
 using namespace OpenASN;
 
 void
 Parser::
-Parse(const std::string& asn)
+Parse(const std::string& asnFilePath)
 {
-  std::cout << "Parsing " << asn << std::endl;
+  std::cout << "Parsing " << asnFilePath << std::endl;
+
+  std::ifstream asn_file(asnFilePath);
+  std::stringstream asn_buf;
+  asn_buf << asn_file.rdbuf();
+
+  // XXX dont use this
+  if (asn_buf.str().empty())
+  {
+    std::cout << "Parsing failed" << std::endl;
+  }
+  else
+  {
+    std::cout << asn_buf.str() << std::endl;
+  }
 }
