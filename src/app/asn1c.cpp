37aa0a61f09f452e5d2f4446221bc725a521c59e
abhijeet080808 <abhijeet080808@gmail.com>
2020-02-06 08:57:18 +0530

    Integrate with cxxopts

--- src/app/asn1c.cpp
+++ src/app/asn1c.cpp
@@ -1,11 +1,32 @@
 #include "parser/Parser.hh"
 
+#include "cxxopts.hpp"
+
 #include <iostream>
 
 using namespace OpenASN;
 
-int main()
+int main(int argc, char* argv[])
 {
+  cxxopts::Options options(argv[0], "C++ ASN.1 Compiler");
+
+  bool apple = false;
+
+  options.add_options()
+    ("a,apple", "an apple", cxxopts::value<bool>(apple));
+
+  try
+  {
+    auto result = options.parse(argc, argv);
+  }
+  catch (const cxxopts::OptionException& e)
+  {
+    std::cout << "Error parsing options: " << e.what() << std::endl;
+    exit(1);
+  }
+
+  std::cout << apple << std::endl;
+
   Parser p;
   p.Parse("Hello");
 }
