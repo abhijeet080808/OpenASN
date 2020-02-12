3a779230663c189d029355f7dca8165509b3cb6a
abhijeet080808 <abhijeet080808@gmail.com>
2020-02-12 21:29:32 +0530

    Add new test case and test BOOLEAN type end to end

---
+++ tests/SimpleTypeTest.cpp
@@ -0,0 +1,44 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+#include "Colored.hh"
+
+TEST_CASE("SimpleTypeTest")
+{
+  SimpleTypeTest::Colored dut;
+
+  SECTION("Checking getters/setters")
+  {
+    CHECK(dut.GetValue() == false);
+
+    dut.SetValue(true);
+    CHECK(dut.GetValue() == true);
+
+    dut.SetValue(false);
+    CHECK(dut.GetValue() == false);
+  }
+
+  SECTION("Checking encode")
+  {
+    std::vector<uint8_t> buf;
+    CHECK(dut.EncodeBER(buf) == true);
+    CHECK(buf == std::vector<uint8_t>{0x01, 0x01, 0x00});
+
+    buf.clear();
+
+    dut.SetValue(true);
+    CHECK(dut.EncodeBER(buf) == true);
+    CHECK(buf == std::vector<uint8_t>{0x01, 0x01, 0xFF});
+  }
+
+  SECTION("Checking decode")
+  {
+    std::vector<uint8_t> buf{0x01, 0x01, 0x00};
+    CHECK(dut.DecodeBER(buf) == true);
+    CHECK(dut.GetValue() == false);
+
+    buf = std::vector<uint8_t>{0x01, 0x01, 0xFF};
+    CHECK(dut.DecodeBER(buf) == true);
+    CHECK(dut.GetValue() == true);
+  }
+}
