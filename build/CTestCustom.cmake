09287088be8cc51c8c043b85155aa04af946b8b0
abhijeet080808 <abhijeet080808@gmail.com>
2020-02-17 12:49:01 +0530

    Change from FetchContent to ExternalProject for all deps Remove Catch2 and use googletest instead - Catch2 does not allow core dumps to be generated!!!

--- build/CTestCustom.cmake
+++
@@ -1,7 +0,0 @@
-set(CTEST_CUSTOM_TESTS_IGNORE
-  ${CTEST_CUSTOM_TESTS_IGNORE}
-  # ignore tests from cxxopts
-  options
-  find-package-test
-  add-subdirectory-test
-)
--- tests/SimpleTypeTest.cpp
+++ tests/SimpleTypeTest.cpp
@@ -1,47 +1,52 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
 
 #include "Colored.hh"
 
 using namespace OpenASN;
+using namespace testing;
 
-TEST_CASE("SimpleTypeTest")
+TEST(SimpleTypeTest, CheckGetSet)
 {
   Colored dut;
+  EXPECT_EQ(dut.GetValue(), false);
 
-  SECTION("Checking getters/setters")
-  {
-    CHECK(dut.GetValue() == false);
+  dut.SetValue(true);
+  EXPECT_EQ(dut.GetValue(), true);
 
-    dut.SetValue(true);
-    CHECK(dut.GetValue() == true);
+  dut.SetValue(false);
+  EXPECT_EQ(dut.GetValue(), false);
+}
 
-    dut.SetValue(false);
-    CHECK(dut.GetValue() == false);
-  }
+TEST(SimpleTypeTest, CheckEncode)
+{
+  Colored dut;
+  std::vector<uint8_t> buf;
+  EXPECT_EQ(dut.EncodeBER(buf), true);
+  EXPECT_THAT(buf, ElementsAre(0x01, 0x01, 0x00));
 
-  SECTION("Checking encode")
-  {
-    std::vector<uint8_t> buf;
-    CHECK(dut.EncodeBER(buf) == true);
-    CHECK(buf == std::vector<uint8_t>{0x01, 0x01, 0x00});
+  buf.clear();
 
-    buf.clear();
+  dut.SetValue(true);
+  EXPECT_EQ(dut.EncodeBER(buf), true);
+  EXPECT_THAT(buf, ElementsAre(0x01, 0x01, 0xFF));
+}
 
-    dut.SetValue(true);
-    CHECK(dut.EncodeBER(buf) == true);
-    CHECK(buf == std::vector<uint8_t>{0x01, 0x01, 0xFF});
-  }
+TEST(SimpleTypeTest, CheckDecode)
+{
+  Colored dut;
+  std::vector<uint8_t> buf{0x01, 0x01, 0x00};
+  EXPECT_EQ(dut.DecodeBER(buf), true);
+  EXPECT_EQ(dut.GetValue(), false);
 
-  SECTION("Checking decode")
-  {
-    std::vector<uint8_t> buf{0x01, 0x01, 0x00};
-    CHECK(dut.DecodeBER(buf) == true);
-    CHECK(dut.GetValue() == false);
+  buf = std::vector<uint8_t>{0x01, 0x01, 0xFF};
+  EXPECT_EQ(dut.DecodeBER(buf), true);
+  EXPECT_EQ(dut.GetValue(), true);
+  EXPECT_EQ(buf.empty(), true);
+}
 
-    buf = std::vector<uint8_t>{0x01, 0x01, 0xFF};
-    CHECK(dut.DecodeBER(buf) == true);
-    CHECK(dut.GetValue() == true);
-    CHECK(buf.empty());
-  }
+int main(int argc, char **argv)
+{
+  InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
